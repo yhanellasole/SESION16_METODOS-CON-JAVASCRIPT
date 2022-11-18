14fc4e881e51c19ff031a804ab83ddace848a60b
yhanellasole <yhanella.lagos.s@gmail.com>
2022-11-17 19:42:56 -0500

    Se agrega index y sesion16

---
+++ sesion16.js
@@ -0,0 +1,59 @@
+/*EJERCICIO 1
+
+let multipli = function(x,y){
+    return x*y
+}
+
+let suma = (x,y) => x+y;
+
+let resta = (x,y) => x-y;
+
+var result = multipli(5,2);
+alert (result);
+
+result = suma (3,4);
+alert (result);
+
+result = resta (10,2);
+alert (result);
+*/
+
+/*EJERCICIO 2 
+
+let pasaraFarenheit = (grado) => {return Fahrenheit = grado * 1.8 +32;};
+
+var result = pasaraFarenheit(22);
+alert (result);
+
+let pasarCelsius = (result) => {return Celsius = (result - 32)* 5/9;};
+
+var resol = pasarCelsius(result);
+alert ((resol).toFixed(2));
+
+*/
+
+
+/*EJERCICIO 3
+
+function suma(){
+
+    let A = +prompt("Ingrese el primer número:");
+
+    let B = +prompt("Ingrese el segundo numero:");
+
+    alert ("La suma de los dos números es:" + " "+ (A + B));
+
+}
+suma()
+
+*/
+
+
+
+
+
+
+
+
+
+
